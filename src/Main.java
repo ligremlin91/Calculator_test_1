a0349cb38da7126184efaca57beb162d80a7f056
Ilya <ligremlin91@gmail.com>
2023-06-29 20:57:32 +0300

    working calculator

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,47 @@
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Укажите первое и второе число:");
-        Scanner inp= new Scanner(System.in);
-        int num1,num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
-        System.out.println("Укажите операцию: 1 Сложение, 2 Вычитание 3 Умножение и 4 Деление:");
-        int choose;
-        choose = inp.nextInt();
-        switch (choose){
-            case 1:
-                System.out.println(add( num1,num2));
-                break;
-            case 2:
-                System.out.println(sub( num1,num2));
-                break;
-            case 3:
-                System.out.println(mult( num1,num2));
-                break;
-            case 4:
-                System.out.println(div( num1,num2));
-                break;
-            default:
-                System.out.println("Невозможное действие");
+        System.out.println("Укажите уравнение:");
+        Scanner inp = new Scanner(System.in);
+        String input = inp.next();
+        var numbers = input.replaceAll("\\D", " $0 ").split(" ");
 
+        int num1 = parseInt(numbers[0]);
+        String choose = numbers[1];
+        int num2 = parseInt(numbers[2]);
 
+        if (num1 >10 || num2 > 10){
+            throw new Error("Заданные числа больше 10!");
         }
 
+        switch (choose) {
+            case "+" -> System.out.println(plus(num1, num2));
+            case "-" -> System.out.println(minus(num1, num2));
+            case "*" -> System.out.println(multiply(num1, num2));
+            case "/" -> System.out.println(divide(num1, num2));
+            default -> System.out.println("Невозможное действие");
+        }
 
-
     }
-    public static int add(int x, int y)
+    public static int plus(int x, int y)
     {
-        int result = x+y;
-        return result;
+        return x+y;
     }
-    public static int sub(int x, int y)
+    public static int minus(int x, int y)
     {
-        int result = x-y;
-        return result;
+        return x-y;
     }
-    public static int mult(int x, int y)
+    public static int multiply(int x, int y)
     {
-        int result = x*y;
-        return result;
+        return x*y;
     }
-    public static int div(int x, int y)
+    public static int divide(int x, int y)
     {
-        int result = x/y;
-        return result;
+        return x/y;
     }
 
 }
